1cb7612823868e29121b4ccd0104b5cd79a4d5c7
Rosa Turner <rmturner1220@gmail.com>
2022-09-09 19:53:59 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Portfolio from './Pages/Portfolio';
+import Resume from './Pages/Resume';
+import Nav from './components';
 
 
 function App() {
+
   return (
     <div className="App">
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/Resume' element={<Resume />} />
+        </Routes>
+      </Router>
 
     </div>
   );
---
+++ src/Pages/Resume/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function index() {
+    return (
+        <div>Resume</div>
+    )
+}
+
+export default index
